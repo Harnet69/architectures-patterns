920081d9a3124606b657e6f36d4bd63f4ab3495c
kokhanau <vital.kokhanau@neptis.pl>
2021-10-03 16:39:13 +0200

    Complete MVP pattern

--- app/src/main/java/com/example/archpattern/mvp/MVPActivity.kt
+++ app/src/main/java/com/example/archpattern/mvp/MVPActivity.kt
@@ -1,12 +1,50 @@
 package com.example.archpattern.mvp
 
 import android.os.Bundle
+import android.util.Log
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Toast
 import com.example.archpattern.BaseActivity
 import com.example.archpattern.R
+import com.example.archpattern.databinding.ActivityMvpactivityBinding
 
-class MVPActivity : BaseActivity() {
+class MVPActivity : BaseActivity(), UniversitiesPresenter.UniversitiesView {
+    private lateinit var binding: ActivityMvpactivityBinding
+    private val universitiesList = arrayListOf<String>()
+    private var adapter: ArrayAdapter<String>? = null
+
+    //TODO inject it instead of instantiate
+    private lateinit var presenter: UniversitiesPresenter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_mvpactivity)
+        binding = ActivityMvpactivityBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        presenter = UniversitiesPresenter(this)
+
+        adapter = ArrayAdapter<String>(this, R.layout.row_layout, R.id.name, universitiesList)
+
+        binding.list.adapter = adapter
+
+        binding.list.onItemClickListener =
+            AdapterView.OnItemClickListener { parent, view, position, id ->
+                Toast.makeText(this, "Click on ${universitiesList[position]}", Toast.LENGTH_SHORT)
+                    .show()
+            }
+
+        setValues(universitiesList)
+    }
+
+    override fun onError(errorMsg: String?){
+        Toast.makeText(this,"Network error, try later", Toast.LENGTH_LONG).show()
+        Log.i("UniReceived", "onSuccess: $errorMsg")
+    }
+
+    override fun setValues(universities: List<String>){
+        universitiesList.clear()
+        universitiesList.addAll(universities)
+        adapter?.notifyDataSetChanged()
     }
 }
---
+++ app/src/main/java/com/example/archpattern/mvp/UniversitiesPresenter.kt
@@ -0,0 +1,43 @@
+package com.example.archpattern.mvp
+
+import android.annotation.SuppressLint
+import com.example.archpattern.model.UniversitiesService
+import com.example.archpattern.model.University
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.observers.DisposableSingleObserver
+import io.reactivex.schedulers.Schedulers
+
+class UniversitiesPresenter(val universitiesView: UniversitiesView) {
+    //TODO inject by Hilt it instead of instantiate
+    private val universitiesService = UniversitiesService()
+    //Interface for interacting with any view
+
+    init {
+        fetchCountries()
+    }
+
+    @SuppressLint("CheckResult")
+    fun fetchCountries() {
+        universitiesService.getRemoteUniversities()
+            // do operation on background thread
+            .subscribeOn(Schedulers.newThread())
+            // observe changes on the main thread
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribeWith(object : DisposableSingleObserver<List<University>>() {
+                @SuppressLint("CheckResult")
+                override fun onSuccess(t: List<University>) {
+                    universitiesView.setValues(t.map { it.name })
+                }
+
+                override fun onError(e: Throwable) {
+                    universitiesView.onError(e.localizedMessage)
+                }
+            })
+    }
+
+    // Separation between view and business logic
+    interface UniversitiesView {
+        fun setValues(universities: List<String>)
+        fun onError(errorMsg: String?)
+    }
+}
